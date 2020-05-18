a0183b4828f4cd11fb51d3301a8ac24e6af1d849
daniel <danielpartogi1996@gmail.com>
2020-05-18 17:45:00 +0700

    Initial Commit

---
+++ Clean_news_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clean_news_app
+//
+//  Created by daniel on 18/05/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
